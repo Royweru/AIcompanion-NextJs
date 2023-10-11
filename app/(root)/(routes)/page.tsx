79458c16b4f4165722bf4ca05dfc80d5767b67b3
Royweru <royezi17@gmail.com>
2023-10-11 10:23:24 +0300

    modified:   app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx 	new file:   app/(auth)/layout.tsx 	modified:   app/(root)/(routes)/page.tsx 	new file:   app/(root)/layout.tsx 	modified:   app/layout.tsx 	new file:   components/MobileSidebar.tsx 	new file:   components/ModeToggle.tsx 	new file:   components/Navbar.tsx 	new file:   components/Sidebar.tsx    new file:   components/theme-provider.tsx 	new file:   components/ui/button.tsx 	new file:   components/ui/dropdown-menu.tsx 	modified:   package-lock.json 	modified:   package.json 	new file:   prisma/schema.prisma

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,6 +1,6 @@
 import { UserButton } from '@clerk/nextjs'
 import React from 'react'
-import { Sparkles } from 'lucide-react'
+
 const RootPage = () => {
   return (
     <div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,30 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import { ClerkProvider } from '@clerk/nextjs'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from "@/components/theme-provider";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'AI-companion',
-  description: 'SaaS AI-companion platform',
-}
+  title: "AI-companion",
+  description: "SaaS AI-companion platform",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <ClerkProvider>
-      <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <html lang="en" suppressHydrationWarning>
+        <body className={inter.className}>
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+            {children}
+            </ThemeProvider>
+        </body>
       </html>
     </ClerkProvider>
-    
-  )
+  );
 }
